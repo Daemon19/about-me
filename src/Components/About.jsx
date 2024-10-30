53a3fb32747a914ac80fe36b62ce6d334a345f6b
Mahatma <90456722+Daemon19@users.noreply.github.com>
2024-10-30 14:31:58 +0000

    Fill in my information

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,25 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/tree-painting.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Gambar berbagai jenis pohon";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Saya seorang siswa bersekolah di SMA Negeri 1 Gianyar. Teknologi-teknologi baru untuk dicoba menjaga saya tetap hidup.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Competitive programming",
+  "Panjat tebing",
+  "Mendaki gunung",
 ];
 
 /**
@@ -44,8 +42,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "Keep small things, small";
 
 const About = () => {
   return (
